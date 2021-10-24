3bc62c5b3bec928da943b354b201d526fadadda0
Kyryl Andreiev <kiril.andreyev@gmail.com>
2021-10-24 21:27:21 +0300

    Миграция на aiogram

--- keyboards.py
+++ keyboards.py
@@ -1,23 +1,23 @@
-from telebot import types
-keyboard = types.ReplyKeyboardMarkup(True)
+from aiogram.types import ReplyKeyboardMarkup
+keyboard = ReplyKeyboardMarkup(True)
 keyboard.row('Баланс')
 keyboard.row('Курс валют')
 keyboard.row('Настройки')
 
-keyboardOpt = types.ReplyKeyboardMarkup(True)
+keyboardOpt = ReplyKeyboardMarkup(True)
 keyboardOpt.row('Управление токеном')
 keyboardOpt.row('Переключить режим откладки')
 keyboardOpt.row('Сбросить настройки')
 keyboardOpt.row('Назад')
 
-keyboardToken = types.ReplyKeyboardMarkup(True)
+keyboardToken = ReplyKeyboardMarkup(True)
 keyboardToken.row('Просмотреть токен')
 keyboardToken.row('Изменить токен')
 keyboardToken.row('Удалить токен')
 keyboardToken.row('Назад')
 
-keyboardDelToken = types.ReplyKeyboardMarkup(True)
+keyboardDelToken = ReplyKeyboardMarkup(True)
 keyboardDelToken.row('Да', 'Нет')
 
-keyboardBack = types.ReplyKeyboardMarkup(True)
+keyboardBack = ReplyKeyboardMarkup(True)
 keyboardBack.row('Назад')
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from telebot import TeleBot as telebot
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext, filters
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from os import path
 from re import match
 from requests import get as rget
-from json import loads as jloads
 from configparser import ConfigParser as configparser
 from tinydb import TinyDB, Query
 from multiprocessing import Process
@@ -16,133 +19,193 @@ from keyboards import *
 config = configparser()
 config.read("config.ini")
 
-bot = telebot(config["MonoApi"]["token"])
+bot = Bot(token=config["MonoApi"]["token"])
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-#Основной блок бота
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    bot.register_next_step_handler(message, send_text)
-    text = message.text
-    #Проверка доступа
-    if message.chat.id == admin_id or any(db.search((find.id == message.chat.id))) is True:
-        if text == '/start':
-            startMessage = '<b>Приветсвую</b>, вы запустили бота для работы с <b>Monobank open API</b>'
-            if db.search((find.id == message.chat.id))[0]["api"] is None:
-                startMessage += '\n<code>Вы не добавили токен, добавьте его через меню</code>'
-            bot.send_message(message.chat.id, startMessage, parse_mode="HTML", reply_markup=keyboard)
-        #Настройки
-        elif text == 'Настройки' or text == '/options':
-            bot.send_message(message.chat.id, "Вы перешли в настройки", parse_mode="HTML", reply_markup=keyboardOpt)
-        #Добавление пользователя
-        elif text == '/adduser' and message.chat.id == admin_id:
-            bot.send_message(message.chat.id, "Введите id пользователя, или перешлите его сообщение", reply_markup=keyboardBack)
-            bot.register_next_step_handler(message, adduser)
-        #Переход в меню "Управление токеном"
-        elif text == 'Управление токеном' or text == '/tokenmenu':
-            bot.send_message(message.chat.id, "Вы перешли в меню управления токеном", reply_markup=keyboardToken)
-        #Изменить токен
-        elif text == 'Изменить токен' or text == '/changetoken':
-            bot.send_message(message.chat.id, "Введите токен, взять вы его можете тут:\nhttps://api.monobank.ua/", reply_markup=keyboardBack)
-            bot.register_next_step_handler(message, changetoken)
-        #Удалить токен
-        elif text == 'Удалить токен' or text == '/deltoken':
-            bot.send_message(message.chat.id, "Вы успешно удалили токен", reply_markup=keyboardToken)
-            db.update({'api': None}, find.id == message.chat.id)
-        #Сбросить все настройки
-        elif text == 'Сбросить настройки' or text == '/reset':
-            bot.send_message(message.chat.id, "Вы уверены что ходите сбросить настройки бота?", reply_markup=keyboardDelToken)
-            bot.register_next_step_handler(message, reset)
-        #Посмотреть токен
-        elif text == 'Просмотреть токен' or text == '/token':
-            dbreq = db.search((find.id == int(message.chat.id)))[0]["api"]
-            if dbreq is not None:
-                result = b64decode(dbreq)
-            else:
-                result = 'Токен отсутствует'
-            bot.send_message(message.chat.id, f'<b>Ваш токен:</b>\n<code>{result}</code>', parse_mode="HTML")
-        #Назад в главное меню
-        elif text == 'Назад':
-            bot.send_message(message.chat.id, "Переход в главное меню", reply_markup=keyboard)
-        #Курс валют
-        elif text == 'Курс валют' or text == '/currency':
-            bot.send_message(message.chat.id, currency(), parse_mode="HTML")
-        elif text == 'Переключить режим откладки' or text == '/debug':
-            user = db.search((find.id == message.chat.id))[0]
-            if user["debug"] is True:
-                db.update({'debug': False}, find.id == message.chat.id)
-                res = 'Отладочная информация выключена'
-            elif user["debug"] is False:
-                db.update({'debug': True}, find.id == message.chat.id)
-                res = 'Отладочная информация включена'
-            bot.send_message(message.chat.id, res)
-        #Баланс
-        elif text == 'Баланс' or text == '/balance' :
-            user = db.search((find.id == message.chat.id))[0]
-            if user["api"] is None:
-                bot.send_message(message.chat.id, 'Вы не добавили токен, добавте его через меню', reply_markup=keyboard)
-            else:
-                headers = {'X-Token': b64decode(user["api"])}
-                delay = user["delay"]
-                if delay < tCurrent():
-                    api = jloads(rget("https://api.monobank.ua/personal/client-info",headers=headers).text)
-                    if "errorDescription" in api:
-                        if user["req"] is None:
-                            result = "no cache\n"
-                            api = {"accounts": [{"currencyCode": 980, "balance": 0, "type": "black"}]}
-                        elif user["req"] is not None:
-                            result = "request error, using cache\n"
-                            api = user["req"]
-                    else:
-                        if user["name"] is None:
-                            db.update({'name': api["name"]}, find.id == message.chat.id)
-                        result = "new\n"
-                        db.update({'req': api}, find.id == message.chat.id)
-                    db.update({'delay': tCurrent()+30}, find.id == message.chat.id)
-                elif delay >= tCurrent() and user["req"] is not None:
-                    result = "cache\n"
-                    api = user["req"]
-                else:
-                    result = "no cache\n"
-                    api = {"accounts": [{"currencyCode": 980, "balance": 0, "type": "black"}]}
-                if user["debug"] is not True:
-                    result = str()
-                bot.send_message(message.chat.id, f'<code>{result}</code>{balance(api)}', parse_mode="HTML", reply_markup=keyboard)
+async def check_access(message):
+    if any(db.search((find.id == message.chat.id))) is True:
+        return True
     else:
-        bot.send_message(message.chat.id, 'Вам сюда нельзя')
+        await message.answer('Вам сюда нельзя')
 
-def adduser(message):
-    if message.text != 'Назад':
-        if message.forward_from is None:
+class token_opt(StatesGroup):
+    main = State()
+    change = State()
+
+class opt(StatesGroup):
+    add = State()
+    reset = State()
+
+@dp.message_handler(commands=['start'])
+async def send_start(message: types.Message):
+    await check_access(message)
+    startMessage = '<b>Приветсвую</b>, вы запустили бота для работы с <b>Monobank open API</b>'
+    if db.search((find.id == message.chat.id))[0]["api"] is None:
+        startMessage += '\n<code>Вы не добавили токен, добавьте его через меню</code>'
+    await message.answer(startMessage, parse_mode="HTML", reply_markup=keyboard)
+
+@dp.message_handler(filters.Text(equals=["назад"], ignore_case=True), state='*')
+@dp.message_handler(commands=["stop", "cancel", "back"], state='*')
+async def cancel(message: types.Message, state: FSMContext):
+    stateCurr = await state.storage.get_state(user=message.chat.id)
+    if stateCurr == 'token_opt:change':
+        keyb = keyboardToken
+    elif stateCurr == 'token_opt:main':
+        keyb = keyboardOpt
+    else:
+        keyb = keyboard
+    await message.answer('Вы вернулись назад', reply_markup=keyb)
+    await state.finish()
+
+@dp.message_handler(filters.Text(equals='Переключить режим откладки', ignore_case=True))
+@dp.message_handler(commands=['debug'])
+async def send_debug(message: types.Message):
+    user = db.search((find.id == message.chat.id))[0]
+    if user["debug"] is True:
+        db.update({'debug': False}, find.id == message.chat.id)
+        res = 'Отладочная информация выключена'
+    elif user["debug"] is False:
+        db.update({'debug': True}, find.id == message.chat.id)
+        res = 'Отладочная информация включена'
+    await message.answer(res)
+
+@dp.message_handler(filters.Text(equals='Настройки', ignore_case=True))
+@dp.message_handler(commands=['options'])
+async def send_options(message: types.Message):
+    if await check_access(message) is True:
+        await message.answer("Вы перешли в настройки", parse_mode="HTML", reply_markup=keyboardOpt)
+
+@dp.message_handler(commands=['adduser'])
+async def send_adduser(message: types.Message):
+    await message.reply("Введите id пользователя, или перешлите его сообщение", reply_markup=keyboardBack)
+    await opt.add.set()
+
+@dp.message_handler(state=opt.add)
+async def send_adduser(message: types.Message, state: FSMContext):
+    if message.forward_from is None:
+        if message.text.isdigit():
             db.insert({'id': int(message.text), 'name': None, "delay": 0, "debug": False, "api": None, "req": None})
-            bot.send_message(message.chat.id, f'Вы успешно добавили пользователя: <code>{message.text}</code>', parse_mode="HTML", reply_markup=keyboard)
+            await message.answer(f'Вы успешно добавили пользователя: <code>{message.text}</code>', parse_mode="HTML", reply_markup=keyboard)
         else:
-            db.insert({'id': message.forward_from.id, 'name': None, "delay": 0, "debug": False, "api": None, "req": None})
-            bot.send_message(message.chat.id, f'Вы успешно добавили пользователя: <code>{message.forward_from.id}</code>', parse_mode="HTML", reply_markup=keyboard)
-def changetoken(message):
-    if message.text != 'Назад':
-        if match("^[A-Za-z0-9_-]*$", message.text) is not None and len(message.text) == 44:
-            headers = {'X-Token': message.text}
-            tTest = jloads(rget("https://api.monobank.ua/personal/client-info",headers=headers).text)
-            if "errorDescription" in tTest:
-                if tTest["errorDescription"] == "Missing one of required headers 'X-Token' or 'X-Key-Id'" or tTest["errorDescription"] == "Unknown 'X-Token'":
-                    bot.send_message(message.chat.id, 'Ошибка, вы ввели неправильный токен', reply_markup=keyboardOpt)
-                else:
-                    db.update({'api': b64encode(message.text)}, find.id == message.chat.id)
-                    bot.send_message(message.chat.id, 'Вы успешно изменили токен', reply_markup=keyboard)
+            await message.answer(f'Вы неверно ввели id', parse_mode="HTML", reply_markup=keyboard)
+    else:
+        db.insert({'id': message.forward_from.id, 'name': None, "delay": 0, "debug": False, "api": None, "req": None})
+        await message.answer(f'Вы успешно добавили пользователя: <code>{message.forward_from.id}</code>', parse_mode="HTML", reply_markup=keyboard)
+    await state.finish()
+
+@dp.message_handler(filters.Text(equals='Управление токеном', ignore_case=True))
+@dp.message_handler(commands=['tokenmenu'])
+async def tokenmenu(message: types.Message):
+    if await check_access(message) is True:
+        await message.answer('Вы перешли в меню управления токеном', reply_markup=keyboardToken)
+        await token_opt.main.set()
+
+@dp.message_handler(state=token_opt.main)
+async def send_token_opt(message: types.Message, state: FSMContext):
+    if message.text == "Просмотреть токен":
+        dbreq = db.search((find.id == int(message.chat.id)))[0]["api"]
+        if dbreq is not None:
+            result = b64decode(dbreq)
+        else:
+            result = 'Токен отсутствует'
+        await message.answer(f'<b>Ваш токен:</b>\n<code>{result}</code>', parse_mode="HTML")
+        await token_opt.main.set()
+    elif message.text == "Удалить токен":
+        db.update({'api': None}, find.id == message.chat.id)
+        await message.answer("Вы успешно удалили токен", reply_markup=keyboardToken)
+        await token_opt.main.set()
+    elif message.text == "Изменить токен":
+        await message.answer("Введите токен, взять вы его можете тут:\nhttps://api.monobank.ua/", reply_markup=keyboardBack)
+        await token_opt.change.set()
+    else:
+        await token_opt.main.set()
+
+@dp.message_handler(state=token_opt.change)
+async def send_token_change(message: types.Message, state: FSMContext):
+    if match("^[A-Za-z0-9_-]*$", message.text) is not None and len(message.text) == 44:
+        headers = {'X-Token': message.text}
+        tTest = jloads(rget("https://api.monobank.ua/personal/client-info",headers=headers).text)
+        if "errorDescription" in tTest:
+            if tTest["errorDescription"] in ["Missing one of required headers 'X-Token' or 'X-Key-Id'", "Unknown 'X-Token'"]:
+                await message.answer('Ошибка, вы ввели неправильный токен')
             else:
                 db.update({'api': b64encode(message.text)}, find.id == message.chat.id)
-                db.update({'name': tTest["name"]}, find.id == message.chat.id)
-                bot.send_message(message.chat.id, 'Вы успешно изменили токен', reply_markup=keyboard)
+                await message.answer('Вы успешно изменили токен', reply_markup=keyboardToken)
         else:
-            bot.send_message(message.chat.id, 'Ошибка, вы ввели неправильный токен', reply_markup=keyboardOpt)
-def reset(message):
-    if message.text == 'Нет':
-        bot.send_message(message.chat.id, 'Вы перешли в настройки', reply_markup=keyboardOpt)
-    elif message.text == 'Да':
-        db.update({'api': None}, find.id == message.chat.id)
-        db.update({'req': None}, find.id == message.chat.id)
-        db.update({'name': None}, find.id == message.chat.id)
-        bot.send_message(message.chat.id, 'Вы сбросили настройки', reply_markup=keyboard)
+            db.update({'api': b64encode(message.text)}, find.id == message.chat.id)
+            db.update({'name': tTest["name"]}, find.id == message.chat.id)
+            await message.answer('Вы успешно изменили токен', reply_markup=keyboardToken)
+            await token_opt.main.set()
+    else:
+        await message.answer('Ошибка, вы ввели неправильный токен')
+        await token_opt.change.set()
+
+@dp.message_handler(filters.Text(equals='Сбросить настройки', ignore_case=True))
+@dp.message_handler(commands=['reset'])
+async def send_reset(message: types.Message):
+    if await check_access(message) is True:
+        await message.answer("Вы уверены что ходите сбросить настройки бота?", reply_markup=keyboardDelToken)
+        await opt.reset.set()
+
+@dp.message_handler(state=opt.reset)
+async def send_reseting(message: types.Message, state: FSMContext):
+    if message.text == "Да":
+        db.update({'api': None, 'req': None, 'name': None}, find.id == message.chat.id)
+        await message.answer('Вы сбросили настройки', reply_markup=keyboard)
+        await state.finish()
+    elif message.text == "Нет":
+        await state.finish()
+        await message.answer('Вы вернулись в настройки', reply_markup=keyboardOpt)
+    else:
+        await opt.reset.set()
+
+@dp.message_handler(filters.Text(equals='Курс валют', ignore_case=True))
+@dp.message_handler(commands=['currency'])
+async def send_currency(message: types.Message):
+    cur = await currency()
+    await message.answer(cur, parse_mode="HTML")
+
+@dp.message_handler(filters.Text(equals='Баланс', ignore_case=True))
+@dp.message_handler(commands=['balance'])
+async def send_balance(message: types.Message):
+    user = db.search((find.id == message.chat.id))[0]
+    if user["api"] is None:
+        await message.answer('Вы не добавили токен, добавте его через меню')
+    else:
+        headers = {'X-Token': b64decode(user["api"])}
+        delay = user["delay"]
+        timenow =  tCurrent()
+        edit_t = False
+        if delay < timenow:
+            msg = await message.answer('<code>Выполняется запрос</code>', parse_mode='HTML')
+            edit_t = True
+            api = rget("https://api.monobank.ua/personal/client-info",headers=headers).json()
+            if "errorDescription" in api:
+                if user["req"] is None:
+                    result = "no cache\n"
+                    api = {"accounts": [{"currencyCode": 980, "balance": 0, "type": "black"}]}
+                elif user["req"] is not None:
+                    result = "request error, using cache\n"
+                    api = user["req"]
+            else:
+                if user["name"] is None:
+                    db.update({'name': api["name"]}, find.id == message.chat.id)
+                result = "new\n"
+                db.update({'req': api}, find.id == message.chat.id)
+            db.update({'delay': tCurrent()+30}, find.id == message.chat.id)
+        elif delay >= timenow and user["req"] is not None:
+            result = "cache\n"
+            api = user["req"]
+        else:
+            result = "no cache\n"
+            api = {"accounts": [{"currencyCode": 980, "balance": 0, "type": "black"}]}
+        if user["debug"] is False:
+            result = str()
+        bal = await balance(api)
+        if edit_t == True:
+            await msg.edit_text(f'<code>{result}</code>{bal}', parse_mode="HTML")
+        else:
+            await message.answer(f'<code>{result}</code>{bal}', parse_mode="HTML")
 
 if __name__ == '__main__':
 
@@ -169,6 +232,5 @@ if __name__ == '__main__':
 
     #Запуск бота
     print("Бот запущен")
-
     cur = Process(target=cureq).start()
-    bot.polling(none_stop=True)
+    executor.start_polling(dp, skip_updates=True)
